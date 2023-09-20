6bd8d0316d7a0065bad26bf22cd82a156300e250
Timo Schneider <timo.schneider@heinrian.com>
2023-09-20 20:03:37 +0200

    added basic register files

--- backend/scripts/register.py
+++ backend/scripts/register.py
@@ -1,28 +1,126 @@
-import monitoring
-import status
+from os.path import exists
+from Interfaces import Item
 
 
-# TTY_PATH = '/dev/ttyS0'
-TTY_PATH = '/dev/null'
+# Exceptions
+class TransmitError(Exception):
+    pass
 
 
-def send(cmd):
-    try:
-        with open(TTY_PATH, "wb") as file:
-            file.write(bytearray.fromhex(cmd.strip(' ')))
-    except (Exception,):
-        monitoring.log(status.WARN, 'Register can\'t exec: ' + cmd)
-    else:
-        monitoring.log(status.INFO, 'Register exec: ' + cmd)
+class PathError(Exception):
+    pass
 
 
-def expose_money():
-    send('1B 70 00 64 32')
+class ConfigError(Exception):
+    msg: str
+
+    def __int__(self, msg: str):
+        self.msg = msg
 
 
-def feed_lines(n=3):
-    send('1B 64 ' + f"{n:02}")
+def to_hex(value: int | str):
+    # for integers
+    if type(value) == int:
+        return f"{value:02x}"
+    # for strings
+    return value.encode('utf-8').hex()
 
 
-cut = ''
-barcode = ''
+class Register:
+    tty: str
+
+    # init new register
+    def __init__(self, tty: str = '/dev/null'):
+        # set local tty path
+        if not exists(tty):
+            raise PathError
+        self._tty = tty
+
+    # transmit hex string to register
+    def _transmit(self, data: str):
+        # check for successful transmit
+        try:
+            with open(self._tty, "wb") as file:
+                # send converted string
+                file.write(bytearray.fromhex(data.strip(' ')))
+        except (Exception,):
+            raise TransmitError
+
+    # reset register to predefined values
+    def _reset(self):
+        # reset
+        self._transmit('10 05 40')
+
+        # TODO: set default config
+
+    def _feed(self, n: int = 1):
+        # check for valid configuration
+        if n not in range(256):
+            raise ConfigError('Can only feed lines in range(0, 256)')
+
+        self._transmit('1B 64 ' + to_hex(n))
+
+    def _cut(self):
+        self._transmit('1B 69')
+
+    def open(self):
+        pass
+
+    def print(self, items: list[Item]):
+        cart = ''
+        total = 0
+        # generate item string
+        for item in items:
+            # line length = 42 chars -> 3 margin, 9 cnt, 17 name, 10 price, 3 margin
+            cart += to_hex('   ')
+            cart += to_hex(f"{item.count:2} Stk.  ")
+            cart += to_hex(f"{item.name:17}")
+            cart += to_hex(f"{item.price * item.count:6.2f} Eur")
+            cart += to_hex('   ')
+
+            # increase cart value
+            total += item.price * item.count
+
+        self._transmit(
+            # start flashing to memory
+            '1D 3A 01'
+            
+            # centered Header
+            '1B 61 01' +
+            to_hex('LMR HS-Esslingen') + '0D' +
+            to_hex('Flandernstrasse 106') + '0D' +
+            to_hex('73732 Esslingen') + '0D' +
+            to_hex(' ') + '0D' +
+            to_hex('Mo - Fr zu Pausenzeiten') + '0D' +
+            to_hex(' ') + '0D' +
+
+            # item list
+            cart +
+
+            # total price
+            to_hex('   --------------------------==========   ') +
+
+            to_hex('                             ') +
+            to_hex(f"{total:6.2f} Eur") +
+            to_hex('   ') +
+
+            # TODO: footer
+
+            # footer
+            '' +
+
+            # end memory flashing
+            '1D 3A' +
+            # print flash
+            '1D 5E 01'
+        )
+
+        # feed newlines to crate bottom margin
+        self._feed(0)
+        self._feed(8)
+
+        # cut recipe
+        self._cut()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._transmit('1B 67')
